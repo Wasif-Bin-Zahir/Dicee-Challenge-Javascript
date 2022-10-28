c18841fefdcc69aa019f8d87d05009e6d5ca6195
Wasif Bin Zahir <106178483+Wasif-Bin-Zahir@users.noreply.github.com>
2022-10-29 00:49:05 +0600

    Add files via upload

---
+++ Dicee Challenge - DOM Manupulation/index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceimage = 'dice' + randomNumber1 + '.png';
+
+var randomImageSource = 'images/' + randomDiceimage;
+
+var image1 = document.querySelectorAll('img')[0];
+
+image1.setAttribute('src', randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomDiceimage2 = 'images/dice' + randomNumber2 + '.png';
+
+var image2 = document.querySelectorAll('img')[1];
+
+image2.setAttribute('src', randomDiceimage2);
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector('h1').innerHTML = 'Player 1 wins !';
+} 
+else if (randomNumber1 < randomNumber2) {
+	document.querySelector('h1').innerHTML = 'Player 2 wins !';
+} 
+else {
+	document.querySelector('h1').innerHTML = 'Draw !';
+}
+
+
+function refreshPage(){
+    window.location.reload();
+}
